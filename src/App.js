2bdab3ff1419a0e238ac562f5a3994bf43db9e53
Lydia K <64658106+lydiakiu@users.noreply.github.com>
2020-09-04 12:37:28 -0700

    Add footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Weather Application</header>
+      <footer>
+        Coded by Lydia K, Open Source on{" "}
+        <a href="https://github.com/lydiakiu/react-weather-app-project-">
+          Git Hub
+        </a>
+      </footer>
     </div>
   );
 }

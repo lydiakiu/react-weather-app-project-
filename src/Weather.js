9f245a4236bff27f3ddec532b52df0c6daec2c7c
Lydia K <64658106+lydiakiu@users.noreply.github.com>
2020-09-04 20:51:31 -0700

    Added Date and Temperature

--- src/Weather.js
+++ src/Weather.js
@@ -1,30 +1,25 @@
 import React, { useState } from "react";
 import "font-awesome/css/font-awesome.min.css";
 import "./Weather.css";
-import Formatdate from "./Formatdate";
+import Weatherinfo from "./Weatherinfo";
 import axios from "axios";
 
 export default function Weather(props) {
-  const [data, setData] = useState({ ready: false });
+  const [data, setData] = useState({});
+  const [loaded, setLoaded] = useState(false);
   const [city, setCity] = useState(props.defaultcity);
 
   function handleResponse(response) {
+    setLoaded(true);
     setData({
-      ready: true,
       temperature: response.data.main.temp,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      maxtemp: response.data.main.temp_max,
-      mintemp: response.data.main.temp_min,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
-  }
-  function search() {
-    const apiKey = "8aaa27a8220932bbfcccd9f6036dc58b";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleSubmit(event) {
@@ -36,7 +31,13 @@ export default function Weather(props) {
     setCity(event.target.value);
   }
 
-  if (data.ready) {
+  function search() {
+    const apiKey = "8aaa27a8220932bbfcccd9f6036dc58b";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
     return (
       <div className="container">
         <form onSubmit={handleSubmit}>
@@ -58,48 +59,9 @@ export default function Weather(props) {
             >
               <i className="fas fa-search"></i>
             </button>
-            <div className="col-4">
-              <button
-                type="button"
-                className="btn btn-outline-secondary shawdow-sm w-100"
-              >
-                My Location
-              </button>
-            </div>
           </div>
         </form>
-
-        <h1>{data.city}</h1>
-
-        <div className="row">
-          <div className="col-3">
-            <Formatdate />
-          </div>
-
-          <div className="col-3">
-            <ul>
-              <li>{Math.round(data.temperature)}°C</li>
-              <li className="text-capitalize">
-                <h2>{data.description}</h2>
-              </li>
-            </ul>
-          </div>
-
-          <div className="col-2">
-            <img
-              src="https://openweathermap.org/img/wn/10d@2x.png"
-              alt="rain"
-            />
-          </div>
-          <div className="col-4">
-            <ul>
-              <li>Humidity: {data.humidity} %</li>
-              <li>Wind Speed: {Math.round(data.wind)} km/h</li>
-              <li>Min Temp: {Math.round(data.mintemp)} °C</li>
-              <li>Max Temp:{Math.round(data.maxtemp)} °C</li>
-            </ul>
-          </div>
-        </div>
+        <Weatherinfo weatherdata={data} />
         <hr />
       </div>
     );
---
+++ src/Weatherinfo.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Temperature from "./Temperature";
+import Formatdate from "./Formatdate";
+
+export default function Weatherinfo(props) {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-8">
+          <h1>{props.weatherdata.city}</h1>
+        </div>
+        <div className="col-4">
+          <img src={props.weatherdata.icon} alt="rain" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <Formatdate />
+        </div>
+
+        <div className="col-4">
+          <ul>
+            <li>
+              <Temperature celcius={props.weatherdata.temperature} />
+            </li>
+            <li className="text-capitalize">
+              <h2>{props.weatherdata.description}</h2>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-4">
+          <ul>
+            <li>Humidity: {props.weatherdata.humidity} %</li>
+            <li>Wind Speed: {Math.round(props.weatherdata.wind)} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
